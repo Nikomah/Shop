96f5957c5ddc6e987dedb87b2b6bdd920b8b1262
Nikomah <d.nikomah@yandex.ru>
2022-05-01 13:36:20 +0300

    Enabled product list by references. Refactoring with Angular 'ngSwitch'. Refactoring styles.

--- frontend/src/app/catalog/catalog.module.ts
+++ frontend/src/app/catalog/catalog.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListComponent } from './list/list.component';
-import { SubcategoryComponent } from './../subcategory/subcategory.component';
-import { ProductComponent } from './../product/product.component';
+import { SubcategoryComponent } from '../subcategory/subcategory.component';
+import { ProductComponent } from '../product/product.component';
 import { RouterModule, Routes } from '@angular/router';
 import {FlexLayoutModule} from "@angular/flex-layout";
 import { MatCardModule  } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-import { ApiService } from './../api.service'
+import { ApiService } from '../api.service'
 
 const routes: Routes = [
   {    path: '', component: ListComponent  },
